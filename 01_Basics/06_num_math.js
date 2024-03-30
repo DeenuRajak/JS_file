88b9c2875e1cab6c7be754ec2084c91ee9e0010b
Deendayal Rajak <125788029+DeenuRajak@users.noreply.github.com>
2024-03-30 08:32:43 +0000

    datatype summary

--- 01_Basics/06_num_math.js
+++ 01_Basics/06_num_math.js
@@ -1,7 +1,7 @@
 const score = 400
 // console.log(score);
 
-const balance = new Number(100)
+const balance = new Number(100)  //Declaration of number
 // console.log(balance);
 
 // console.log(balance.toString().length);
@@ -12,7 +12,7 @@ const otherNumber = 123.8966
 // console.log(otherNumber.toPrecision(4));
 
 const hundreds = 1000000
-// console.log(hundreds.toLocaleString('en-IN'));
+console.log(hundreds.toLocaleString('en-IN'));
 
 // +++++++++++++ Maths +++++++++++++++++++++++++++++
 
